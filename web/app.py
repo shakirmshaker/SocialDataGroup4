1917545133d6dce9d4ebd77a90ebf306cdcb55d4
Shakir M. Shaker <shakir.m.shaker@gmail.com>
2024-04-21 21:47:39 +0200

    added interaction to geo plot

--- web/app.py
+++ web/app.py
@@ -1,20 +1,51 @@
 import streamlit as st
+import pandas as pd
 import os
 
-def main():
-    st.write(os.getcwd())
-    st.title('Hello, Streamlit!')
+st.set_page_config(
+    page_title="Social Data Analysis | Solar Energy Project",
+    page_icon="🌞",
+    layout="wide",
+    initial_sidebar_state="auto",
+)
 
-    menu = ["Home", "About", 'Test']
-    choice = st.sidebar.selectbox("Menu", menu)
+data = pd.read_csv("../final/data/dfMerged.csv")
 
-    if choice == "Home":
-        st.subheader("Home")
-        # Your code goes here
+st.sidebar.title('Social Data Analysis | Solar Energy Project')
+st.sidebar.write("Welcome to the Solar Energy Project. This project aims to analyze solar energy data from the danish company EasyGreen.")
+st.sidebar.write("Please select the data you would like to analyze from the sidebar.")
 
-    elif choice == "About":
-        st.subheader("About")
-        # Your code goes here
+choice = st.sidebar.selectbox("Visualization", ["Geo Development", "Production Development", "Social Data Analysis"])
 
-if __name__ == "__main__":
-    main()
+# create day, month, year columns from usage_date
+data['usage_date'] = pd.to_datetime(data['usage_date'])
+data['day'] = data['usage_date'].dt.day
+data['month'] = data['usage_date'].dt.month
+data['year'] = data['usage_date'].dt.year
+
+if choice == "Geo Development":
+
+    st.header("Geo Development")  
+    
+    idx = data.groupby('user_id')['usage_date'].idxmin()
+    first_usage_df = data.loc[idx]
+    first_usage_df = first_usage_df.dropna(subset=['latitude', 'longitude'])
+
+    first_usage_df['month_year'] = first_usage_df['usage_date'].dt.strftime('%m-%Y')
+    df_sorted = first_usage_df.sort_values(by='usage_date')
+    sorted_month_year_options = df_sorted['month_year'].unique()
+
+    selected_month_year = st.sidebar.select_slider("Select Month-Year", options=sorted_month_year_options, format_func=lambda x: x)
+
+    selected_date = pd.to_datetime(selected_month_year, format='%m-%Y')
+
+    # Filter DataFrame to only include rows with date less than or equal to selected date
+    filtered_df = first_usage_df[first_usage_df['usage_date'] <= selected_date]
+    
+    st.map(filtered_df)
+
+
+st.sidebar.download_button("Download Complete Notebook", open("../final/explainer.ipynb").read(), 'SolarEnergyProject.ipynb')
+
+# read notebook into streamlit
+# st.write("Notebook")
